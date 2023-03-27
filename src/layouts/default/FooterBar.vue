0d81ec7a6f6a33455006578a8f80de3c611ea73c
Frederic Marvin Abraham <mail@fabraham.dev>
2023-03-28 00:48:56 +0200

    Continue Dashboard

---
+++ src/layouts/default/FooterBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container class="my-0 py-0">
+    <v-responsive class="d-flex">
+      <v-footer
+        class="bg-indigo-lighten-1 text-center d-flex flex-column"
+      >
+        <v-divider></v-divider>
+        <div>
+          &copy; {{ new Date().getFullYear() }} — <strong>Gesangbuch Dashboard</strong>
+        </div>
+      </v-footer>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
