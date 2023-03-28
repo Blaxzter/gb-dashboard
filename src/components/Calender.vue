7765d6fb6d401d07fb113eb1987e986c2a3819c0
Frederic Marvin Abraham <mail@fabraham.dev>
2023-03-28 09:17:08 +0200

    Events and deadlines on top

--- src/components/Calender.vue
+++ src/components/Calender.vue
@@ -1,6 +1,34 @@
 <template>
   <v-container>
-    <div class="text-h4">
+
+    <div class="text-h4 mb-5">
+      Nächste Deadlines
+    </div>
+    <div v-for="event in filteredEvents" :key="event.id" class="mb-4">
+      <div class="border rounded d-flex pa-3"
+           :style="event.type === 'deadline' ? 'border: 1px solid red !important;' : 'border: 1px solid black ; '">
+        <div class="d-flex justify-center align-center mr-5 ml-2">
+          <v-icon v-if="event.type === 'deadline'" icon="mdi-lightning-bolt" color="red"/>
+          <v-icon v-else-if="event.type === 'normal'" icon="mdi-calendar-blank"/>
+        </div>
+        <v-divider vertical class="mr-5" />
+        <div>
+          <div class="text-h6">
+            {{ event.name }}
+          </div>
+          <div class="text-caption mb-1">
+            Termin: {{ toDate(event.date) }}
+          </div>
+          <div class="text-body-1">
+            {{ event.description }}
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <v-divider class="my-10" />
+
+    <div class="text-h4  mb-5">
       Kalender
     </div>
     <vue-cal style="height: 500px" active-view="month" locale="de"/>
@@ -18,14 +46,33 @@ export default {
   components: {VueCal},
   data: () => ({
     date: null,
-    trip: {
-      name: '',
-      location: null,
-      start: null,
-      end: null,
-    },
-    locations: ['Australia', 'Barbados', 'Chile', 'Denmark', 'Ecuador', 'France'],
+    events: [
+      {
+        id: 1,
+        date: 1679984928,
+        name: "Dashboard Creation",
+        description: "Frederic Arrbeitet ganz viel damit das dash board fertig wird.",
+        type: 'normal'
+      },
+      {
+        id: 2,
+        date: 1680192000,
+        name: "Dashboard Deadline",
+        description: "Frederic Arbeitet ganz viel, damit das Dash board fertig wird.",
+        type: 'deadline'
+      },
+    ]
   }),
+  computed: {
+    filteredEvents() {
+      return this.events;
+    }
+  },
+  methods: {
+    toDate(unix_timestamp) {
+      return new Date(unix_timestamp * 1000).toLocaleDateString("de")
+    }
+  }
 }
 
 </script>
