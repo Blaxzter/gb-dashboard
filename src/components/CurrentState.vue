7d07b72c113f71047ee0d26aa6c4f678220d0b7e
Frederic Marvin Abraham <mail@fabraham.dev>
2023-03-28 23:47:09 +0200

    Add more  dashboard components and docker stuff

---
+++ src/components/CurrentState.vue
@@ -0,0 +1,115 @@
+<template>
+
+  <v-container>
+    <div class="text-h4 mb-6">
+      Aktueller Stand
+    </div>
+    <v-row>
+      <v-col cols="6">
+        <v-card class="pa-3">
+          <div class="text-h5">
+            Lieder
+          </div>
+          <div>
+            <Doughnut :data="song_chart_data" :options="options"/>
+          </div>
+        </v-card>
+      </v-col>
+      <v-col cols="6">
+      <v-card class=" pa-3">
+        <div class="text-h5">
+          Arbeitsaufträge der Arbeitskreise
+        </div>
+        <div>
+          <Doughnut :data="work_chart_data" :options="options"/>
+        </div>
+      </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+
+</template>
+
+<script>
+
+import _ from 'lodash';
+
+import {mapStores} from 'pinia'
+import {useAppStore} from "@/store/app";
+
+import {ArcElement, Chart as ChartJS, Legend, Tooltip} from 'chart.js'
+import {Doughnut} from 'vue-chartjs'
+
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+export default {
+  name: "CurrentState",
+  components: {
+    Doughnut
+  },
+  data: () => ({
+    tab: null,
+    store: useAppStore(),
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    },
+  }),
+  computed: {
+    ...mapStores(useAppStore),
+    songs() {
+      return this.store.gesangbuchlied;
+    },
+    work_orders() {
+      return this.store.auftraege;
+    },
+    song_chart_data() {
+      return {
+        labels: ['Veröffentlicht', 'Entwurf', 'Archiviert'],
+        datasets: [
+          {
+            backgroundColor: ['#41B883', '#00D8FF', '#DD1B16'],
+            data: this.song_data_list
+          }
+        ]
+      }
+    },
+    work_chart_data() {
+      return {
+        labels: ['Text', 'Melodie', 'Jugend', 'Kinder', 'IT', 'Kleiner Kreis'],
+        datasets: [
+          {
+            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF', '#FF9F40'],
+            data: this.work_orders_data_list
+          }
+        ]
+      }
+    },
+    song_data_list() {
+      const song_data = _.countBy(this.songs, (ele) => ele.status)
+      return [
+        _.get(song_data, 'published', 0),
+        _.get(song_data, 'draft', 0),
+        _.get(song_data, 'archived', 0),
+      ]
+    },
+    work_orders_data_list() {
+      console.log(this.work_orders)
+      const work_orders_data = _.countBy(this.work_orders, (ele) => ele.arbeitskreisId)
+      console.log(work_orders_data)
+      return [
+        _.get(work_orders_data, '1', 0),
+        _.get(work_orders_data, '2', 0),
+        _.get(work_orders_data, '3', 0),
+        _.get(work_orders_data, '4', 0),
+        _.get(work_orders_data, '5', 0),
+        _.get(work_orders_data, '6', 0),
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
