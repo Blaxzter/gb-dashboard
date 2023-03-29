0c56ef5a3d2be5722d432e7bd1cb31cfa92d7845
Frederic Marvin Abraham <mail@fabraham.dev>
2023-03-29 20:12:54 +0200

    Update arbeitsauftrags tabelle

--- src/components/WorkOrders.vue
+++ src/components/WorkOrders.vue
@@ -18,35 +18,15 @@
       <v-card-text>
         <v-window v-model="tab">
           <v-window-item value="one">
-            <v-table>
-              <thead>
-              <tr>
-                <th class="text-left" v-for="table_head in table_header" :key="table_head">
-                  {{ table_head }}
-                </th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr
-                v-for="auftrag in auftraege"
-                :key="auftrag.id"
-              >
-                <td>{{ auftrag.arbeitskreisId }}</td>
-                <td>{{ auftrag.abgabetermin }}</td>
-                <td>{{ auftrag.textId }}</td>
-                <td>{{ auftrag.melodieId }}</td>
-                <td>{{ auftrag.anmerkung }}</td>
-              </tr>
-              </tbody>
-            </v-table>
+            <AuftragTable :auftraege="auftraege"/>
           </v-window-item>
 
           <v-window-item value="two">
-            {{ auftraege }}
+            <AuftragTable :auftraege="auftraege"/>
           </v-window-item>
 
           <v-window-item value="three">
-            Three
+            <AuftragTable :auftraege="auftraege"/>
           </v-window-item>
         </v-window>
       </v-card-text>
@@ -59,13 +39,14 @@
 
 import { mapStores } from 'pinia'
 import {useAppStore} from "@/store/app";
+import AuftragTable from "@/components/AuftragTable.vue";
 
 export default {
   name: "WorkOrders",
+  components: {AuftragTable},
   data: () => ({
     tab: null,
     store: useAppStore(),
-    table_header: ['Arbeitskreis', 'Abgabetermin', 'Text', 'Melodie', 'Anmerkung'],
     desserts: [
       {
         name: 'Frozen Yogurt',
