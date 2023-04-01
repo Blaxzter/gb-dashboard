ed7d144b071b61dbb23671bd6ecf6a0304761dae
Frederic Marvin Abraham <mail@fabraham.dev>
2023-04-01 16:33:58 +0200

    Add formular scaffold

---
+++ src/components/upload/VuetifyDatepicker.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="text-center">
+    <v-menu v-model="menu" :close-on-content-click="false" location="top">
+      <template v-slot:activator="{ props }">
+        <v-text-field
+          v-bind="props"
+          outlined
+          :label="label"
+          v-model="visible_date"
+          @input="date_changed"
+          append-icon="mdi-calendar"
+        ></v-text-field>
+      </template>
+
+      <div style="height: 278px">
+        <VDatePicker
+          ref="date_picker"
+          class="w-100"
+          :model-value="selected_date"
+          mode="date"
+          @dayclick="day_clicked($event)"
+        ></VDatePicker>
+      </div>
+    </v-menu>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  props: {
+    label: String,
+    date: Date,
+  },
+  data: () => ({
+    menu: false,
+    visible_date: null,
+    hidden_date: null,
+  }),
+  emits: ["update:date"],
+  computed: {
+    selected_date() {
+      return this.hidden_date
+    },
+  },
+  methods: {
+
+    async date_changed(event) {
+      let dateString = event.target.value;
+      if (dateString.length === 10) {
+        const [day, month, year] = dateString.split(".");
+        this.hidden_date = new Date(year, month - 1, day);
+        await this.$refs.date_picker.move(this.hidden_date)
+        this.$emit("update:date", this.hidden_date);
+      }
+    },
+    day_clicked(event) {
+      let visibleDate = moment(event.startDate).format("DD.MM.YYYY");
+      this.$emit("update:date",  new Date(event.startDate));
+      this.visible_date = visibleDate;
+      this.hidden_date = new Date(event.startDate);
+      this.menu = false;
+    },
+  },
+};
+</script>
