87acf44a200cd4c2937a5c0674f05c8b6c24b11a
Frederic Marvin Abraham <mail@fabraham.dev>
2023-04-02 00:40:22 +0200

    Finish form

---
+++ src/components/upload/MelodieData.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="12" class="pb-0">
+        <v-text-field
+          :model-value="title"
+          @change="$emit('update:title', $event.target.value)"
+          label="Melodie Titel"
+          hide-details="auto"
+          class="mb-3"
+          required
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12" class="pb-0">
+        <v-file-input show-size label="Noten Datein"> </v-file-input>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12" md="6" class="py-0">
+        <v-text-field
+          :model-value="quelle"
+          @change="$emit('update:quelle', $event.target.value)"
+          label="Quelle"
+          hide-details="auto"
+          class="mb-3"
+          required
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12" md="6" class="py-0">
+        <v-text-field
+          :model-value="quellelink"
+          @change="$emit('update:quellelink', $event.target.value)"
+          label="Melodie Quelle Link"
+          hide-details="auto"
+          class="mb-3"
+          required
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" class="pt-0">
+        <v-textarea
+          :model-value="anmerkung"
+          @change="$emit('update:anmerkung', $event.target.value)"
+          label="Melodie Anmerkung"
+          hide-details="auto"
+          class="mb-3"
+          required
+        ></v-textarea>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MelodieData",
+  props: {
+    title: String,
+    noten: File,
+    quelle: String,
+    quellelink: String,
+    anmerkung: String,
+  },
+};
+</script>
+
+<style scoped></style>
