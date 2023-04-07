8db75af250cb2f98094053d89d2b98bea6833cb8
Frederic Abraham <mail@fabraham.dev>
2023-04-07 17:52:06 +0200

    New Design

---
+++ src/components/OldDashboardComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive>
+      <UploadFormular/>
+
+      <v-divider class="my-8"/>
+
+      <CurrentState/>
+
+      <v-divider class="my-8"/>
+
+      <Calender/>
+
+      <v-divider class="my-8"/>
+
+      <WorkOrders/>
+
+      <v-divider class="my-8"/>
+
+      <SongOverview/>
+
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+
+import Calender from "@/components/Calender.vue";
+import WorkOrders from "@/components/Workorders/WorkOrders.vue";
+import CurrentState from "@/components/CurrentState.vue";
+import UploadFormular from "@/components/upload/UploadFormular.vue";
+import SongOverview from "@/components/SongOverview.vue";
+
+</script>
---
+++ src/views/UploadFormView.vue
@@ -0,0 +1,7 @@
+<template>
+  <UploadFormular/>
+</template>
+
+<script setup>
+import UploadFormular from "@/components/upload/UploadFormular.vue";
+</script>
