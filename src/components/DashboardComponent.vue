dd5cdc24a5d43a52a4c41f6c261db9cd3c5efb74
Frederic Abraham <mail@fabraham.dev>
2023-04-07 17:52:06 +0200

    New Design

--- src/components/DashboardComponent.vue
+++ src/components/DashboardComponent.vue
@@ -1,34 +1,9 @@
 <template>
-  <v-container class="fill-height">
-    <v-responsive>
-      <UploadFormular/>
-
-      <v-divider class="my-8"/>
-
-      <CurrentState/>
-
-      <v-divider class="my-8"/>
-
-      <Calender/>
-
-      <v-divider class="my-8"/>
-
-      <WorkOrders/>
-
-      <v-divider class="my-8"/>
-
-      <SongOverview/>
-
-    </v-responsive>
-  </v-container>
+  <CurrentState/>
 </template>
 
 <script setup>
 
-import Calender from "@/components/Calender.vue";
-import WorkOrders from "@/components/WorkOrders.vue";
 import CurrentState from "@/components/CurrentState.vue";
-import UploadFormular from "@/components/UploadFormular.vue";
-import SongOverview from "@/components/SongOverview.vue";
 
 </script>
