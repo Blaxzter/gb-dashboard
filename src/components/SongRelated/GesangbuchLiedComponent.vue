5e2a9cf65637a63bb1adb05d7a046b90afff86f1
Frederic Marvin Abraham <mail@fabraham.dev>
2023-04-08 15:10:24 +0200

    Added dialogs for text melodie and song

---
+++ src/components/SongRelated/GesangbuchLiedComponent.vue
@@ -0,0 +1,147 @@
+<template>
+  <v-card class="pa-3">
+    <v-card-title>
+      <v-card-title class="d-flex justify-space-between pb-0 align-center">
+        <div>
+          {{ selected_song.titel }}
+        </div>
+        <div>
+          <v-tooltip text="Mehr Text Informationen" location="bottom" v-if="selected_song.text">
+            <template v-slot:activator="{ props }">
+              <v-btn icon="mdi-text-box" v-bind="props" variant="text" @click="text_dialog = true"/>
+            </template>
+          </v-tooltip>
+          <v-tooltip text="Mehr Melodie Informationen" location="bottom" v-if="selected_song.melodie">
+            <template v-slot:activator="{ props }">
+              <v-btn icon="mdi-music" v-bind="props" variant="text" @click="melodie_dialog = true"/>
+            </template>
+          </v-tooltip>
+        </div>
+      </v-card-title>
+    </v-card-title>
+    <v-card-text class="pt-0">
+      <v-carousel
+        v-if="selected_song.melodie.files.length"
+        :show-arrows="selected_song.melodie.files?.length <= 1 ? false : 'hover'"
+        hide-delimiter-background
+        height="300"
+        v-model="pdf_carousel_model"
+      >
+        <v-carousel-item
+          v-for="(file, i) in selected_song.melodie.files"
+          :key="i"
+          :src="file.type.includes('image') ? getImgUrl(file.id) : null"
+        >
+          <div class="d-flex align-center justify-center fill-height bg-grey" v-if="file.type === 'application/pdf'">
+            <vue-pdf-embed
+              height="300"
+              :source="getPdfUrl(file.id)"
+              @click="fullscreen_pdf($event, file)"
+            />
+          </div>
+        </v-carousel-item>
+      </v-carousel>
+      <div class="d-flex flex-row text-subtitle-2" style="max-width: 600px;" v-if="selected_song?.melodie.files.length">
+        <div class="me-3">
+          Dateiname:
+        </div>
+        <div>
+          {{selected_song?.melodie.files[pdf_carousel_model]?.title}}
+        </div>
+      </div>
+
+      <div class="text-h6 mx-auto">
+        Strophen
+      </div>
+      <div
+        style="max-width: 500px;"
+        class="d-flex flex-row mb-4 mx-auto"
+        v-for="(strophe, index) in selected_song?.text.strophenEinzeln"
+        :key="index"
+      >
+        <div class="me-2">{{ index + 1 }}.</div>
+        <div>
+          {{ strophe.strophe }}
+        </div>
+      </div>
+
+      <div v-for="(author_source, index_1) in [{name: 'Text', src: selected_song?.text?.authors}, {name: 'Melodie', src: selected_song?.melodie?.authors}]" :key="index_1">
+        <div v-if="author_source">
+          <div class="text-subtitle-1 font-weight-medium">
+            {{ author_source.name }} Author
+          </div>
+          <div
+            class="d-flex flex-row mb-4"
+            v-for="(author, index) in author_source.src"
+            :key="index"
+          >
+            <div class="me-2">{{ index + 1 }}.</div>
+            <div>
+              {{ author.vorname }} {{ author.nachname }} {{
+                author.geburtsjahr || author.sterbejahr ? ` (${author.geburtsjahr ?
+                  author.geburtsjahr : ''} - ${author.sterbejahr ? author.sterbejahr : '?'})` : ''
+              }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </v-card-text>
+  </v-card>
+
+  <v-dialog v-model="noten_dialog" style="height: 100vh; width: 100vw">
+    <div class="position-relative" style="overflow: scroll">
+      <v-btn icon="mdi-close" @click="noten_dialog = false" class="position-fixed ma-10" style="z-index: 10000; right: 0"></v-btn>
+      <vue-pdf-embed
+        v-if="selected_file"
+        :source="getPdfUrl(selected_file.id)"
+        :page="1"
+      />
+    </div>
+  </v-dialog>
+
+  <v-dialog v-model="text_dialog" width="700">
+    <TextDialog :text="selected_song.text" />
+  </v-dialog>
+  <v-dialog v-model="melodie_dialog" width="700">
+    <MelodieDialog :melodie="selected_song.melodie" />
+  </v-dialog>
+</template>
+
+<script>
+import VuePdfEmbed from "vue-pdf-embed";
+import TextDialog from "@/components/SongRelated/TextDialog.vue";
+import MelodieDialog from "@/components/SongRelated/MelodieDialog.vue";
+
+export default {
+  name: "GesangbuchLiedComponent",
+  components: {MelodieDialog, TextDialog, VuePdfEmbed},
+  props: {
+    selected_song: Object,
+  },
+  data: () => ({
+    noten_dialog: false,
+    text_dialog: false,
+    melodie_dialog: false,
+    selected_file: null,
+    pdf_carousel_model: 0
+  }),
+  methods: {
+    getPdfUrl(file_id) {
+      return `https://gb26.***REMOVED***/assets/${file_id}.pdf`;
+    },
+    getImgUrl(file_id) {
+      return `${import.meta.env.VITE_BACKEND_URL}/assets/${file_id}`;
+    },
+    fullscreen_pdf(event, file) {
+      console.log(event)
+      console.log(file)
+      this.selected_file = file;
+      this.noten_dialog = true;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
