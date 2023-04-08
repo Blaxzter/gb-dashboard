0be743ba7f63a7dfbb1d52e26f288a55f959da49
Frederic Marvin Abraham <mail@fabraham.dev>
2023-04-08 03:56:57 +0200

    Add Gesangbuch click dings

--- src/components/Workorders/WorkOrders.vue
+++ src/components/Workorders/WorkOrders.vue
@@ -1,63 +1,118 @@
 <template>
+  <div class="d-flex align-center justify-space-between  mb-6">
+    <div class="text-h4">Arbeitsaufträge</div>
+    <div>
+      <v-btn-toggle v-model="selected_tabs" elevation="1" color="success" class="me-3">
+        <v-btn prepend-icon="mdi-arm-flex">Auftrags Typen</v-btn>
+        <v-btn prepend-icon="mdi-shape">Kategorien</v-btn>
+      </v-btn-toggle>
 
-  <v-container>
-    <div class="text-h4 mb-6">
-      Arbeitsaufträge
+      <v-btn-toggle v-model="display_style" elevation="1" color="primary">
+        <v-btn>
+          <v-icon>
+            mdi-card-multiple-outline
+          </v-icon>
+        </v-btn>
+        <v-btn>
+          <v-icon>
+            mdi-table
+          </v-icon>
+        </v-btn>
+      </v-btn-toggle>
     </div>
+  </div>
 
-    <v-card height="500px">
-      <v-tabs
-        v-model="tab"
-        fixed-tabs
-      >
-        <v-tab value="one">Texte, die Melodie benötigen</v-tab>
-        <v-tab value="two">Texte, die Bearbeitung benötigen</v-tab>
-        <v-tab value="three">Melodien, die Bearbeitungen benötigen</v-tab>
-      </v-tabs>
+  <v-tabs v-model="tab" fixed-tabs>
+    <v-tab value="alle">Alle</v-tab>
+    <v-tab v-for="(label, index) in current_tabs" :key="index" :value="index">
+      {{ selected_tabs === 1 ? label : auftrag_type_to_name[label] }}
+    </v-tab>
+  </v-tabs>
 
-      <v-card-text>
-        <v-window v-model="tab">
-          <v-window-item value="one">
-            <AuftragTable :auftraege="auftraege"/>
-          </v-window-item>
-
-          <v-window-item value="two">
-            <AuftragTable :auftraege="auftraege"/>
-          </v-window-item>
-
-          <v-window-item value="three">
-            <AuftragTable :auftraege="auftraege"/>
-          </v-window-item>
-        </v-window>
-      </v-card-text>
-    </v-card>
-  </v-container>
-
+  <v-card-text>
+    <v-window v-model="tab">
+      <v-window-item value="alle">
+        <AuftragCards :auftraege="auftraege" v-if="display_style === 0"/>
+        <AuftragTable :auftraege="auftraege" v-else-if="display_style === 1"/>
+      </v-window-item>
+      <v-window-item v-for="(label, index) in current_tabs" :key="index" :value="index">
+        <AuftragCards :auftraege="current_group[label]" v-if="display_style === 0"/>
+        <AuftragTable :auftraege="current_group[label]" v-else-if="display_style === 1"/>
+      </v-window-item>
+    </v-window>
+  </v-card-text>
 </template>
 
 <script>
-
-import { mapStores } from 'pinia'
+import {mapStores} from "pinia";
 import {useAppStore} from "@/store/app";
 import AuftragTable from "@/components/Workorders/AuftragTable.vue";
+import AuftragCards from "@/components/Workorders/AuftragCards.vue";
+import {auftrag_type_to_name} from "@/assets/js/utils";
+import _ from "lodash";
 
 export default {
   name: "WorkOrders",
-  components: {AuftragTable},
+  components: {
+    AuftragTable,
+    AuftragCards,
+  },
   data: () => ({
     tab: null,
     store: useAppStore(),
+    display_style: 0,
+    selected_tabs: 0,
+    group_by_auftrag: {},
+    group_by_arbeitskreis: {},
   }),
+  watch: {
+    selected_tabs() {
+      this.tab = 0
+    }
+  },
+  mounted() {
+
+    let auftragsartText =  _.groupBy(this.auftraege, (elem) => elem.auftragsartText)
+    let auftragsartMelodie =  _.groupBy(this.auftraege, (elem) => elem.auftragsartMelodie)
+
+    let status = []
+    if ('sonstiges' in auftragsartText) status.push(...auftragsartText['sonstiges'])
+    if ('sonstiges' in auftragsartMelodie) status.push(...auftragsartMelodie['sonstiges'])
+
+    let rueckfrageAutor = []
+    if ('rueckfrageAutor' in auftragsartText) status.push(...auftragsartText['rueckfrageAutor'])
+    if ('rueckfrageAutor' in auftragsartMelodie) status.push(...auftragsartMelodie['rueckfrageAutor'])
+
+    delete auftragsartText['auftragsartText']
+    delete auftragsartText['sonstiges']
+    delete auftragsartMelodie['auftragsartText']
+    delete auftragsartMelodie['sonstiges']
+
+    this.group_by_auftrag = {status: status, rueckfrageAutor: rueckfrageAutor, ...auftragsartText, ...auftragsartMelodie}
+    this.group_by_arbeitskreis = _.groupBy(this.auftraege, (elem) => elem.arbeitskreis_name)
+  },
   computed: {
+    auftrag_type_to_name() {
+      return auftrag_type_to_name
+    },
     ...mapStores(useAppStore),
     auftraege() {
       return this.store.auftraege;
     },
-
-  }
-}
+    categories() {
+      return this.store.auftrags_categories
+    },
+    current_tabs() {
+      return this.selected_tabs === 1 ? this.categories : this.auftrag_types;
+    },
+    current_group() {
+      return this.selected_tabs === 1 ? this.group_by_arbeitskreis : this.group_by_auftrag;
+    },
+    auftrag_types() {
+      return this.store.auftrags_typen
+    }
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
