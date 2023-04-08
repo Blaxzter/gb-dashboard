5d374a7344be404738d8e2fa3c0c6034040012d0
Frederic Marvin Abraham <mail@fabraham.dev>
2023-04-08 15:10:24 +0200

    Added dialogs for text melodie and song

---
+++ src/components/SongRelated/TextDialog.vue
@@ -0,0 +1,104 @@
+<template>
+  <v-card class="pa-3">
+    <v-card-title>
+      <v-card-title class="d-flex justify-space-between">
+        <div>
+          {{ text.titel }}
+        </div>
+        <div>
+          {{ status_mapping[text.status] }}
+        </div>
+      </v-card-title>
+    </v-card-title>
+    <v-card-text class="px-8">
+      <div v-if="text?.strophenEinzeln?.length" class="mb-2">
+        <div class="text-h6 mb-2">Strophen</div>
+        <div
+          style="max-width: 500px"
+          class="d-flex flex-row mb-1 mx-auto bg-grey-lighten-4 pa-2 rounded"
+          v-for="(strophe, index) in text?.strophenEinzeln"
+          :key="index"
+        >
+          <div class="me-2">{{ index + 1 }}.</div>
+          <div>
+            {{ strophe.strophe }}
+          </div>
+        </div>
+      </div>
+
+      <div v-if="text?.anmerkung" class="mb-2">
+        <div class="text-subtitle-1 font-weight-medium">Anmerkung:</div>
+        <div>
+          {{ text?.anmerkung }}
+        </div>
+      </div>
+
+      <div v-if="text?.rueckfrageAutor" class="mb-2">
+        <div class="text-subtitle-1 font-weight-medium">Rückfrage Author:</div>
+        <div>
+          {{ text?.rueckfrageAutor }}
+        </div>
+      </div>
+
+      <div v-if="text?.quelle" class="mb-2">
+        <div class="text-subtitle-1 font-weight-medium">Quelle:</div>
+        <div>
+          {{ text?.quelle }}
+        </div>
+      </div>
+
+      <div v-if="text?.quelllink" class="mb-2">
+        <div class="text-subtitle-1 font-weight-medium">Quelle Link:</div>
+        <div class="d-flex flex-row align-center">
+          <v-icon
+            icon="mdi-open-in-new"
+            size="tiny"
+            class="me-2"
+            color="blue-darken-3"
+          />
+          <a :href="text?.quelllink" target="_blank">
+            {{ text?.quelllink }}
+          </a>
+        </div>
+      </div>
+
+      <div class="text-subtitle-1 font-weight-medium">Authoren</div>
+      <div
+        class="d-flex flex-row mb-4"
+        v-for="(author, index) in text?.authors"
+        :key="index"
+      >
+        <div class="me-2">{{ index + 1 }}.</div>
+        <div>
+          {{ author.vorname }} {{ author.nachname }}
+          {{
+            author.geburtsjahr || author.sterbejahr
+              ? ` (${author.geburtsjahr ? author.geburtsjahr : ""} - ${
+                  author.sterbejahr ? author.sterbejahr : "?"
+                })`
+              : ""
+          }}
+        </div>
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import { status_mapping } from "../../assets/js/utils";
+
+export default {
+  name: "TextDialog",
+  computed: {
+    status_mapping() {
+      return status_mapping;
+    },
+  },
+  components: {},
+  props: {
+    text: Object,
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Workorders/AuftragsCard.vue
@@ -0,0 +1,146 @@
+<template>
+  <v-card class="mb-4" :class="`${arbeitskreis_name_to_css(auftrag.arbeitskreis_name)}-bg`">
+    <template v-slot:title>
+      <div class="d-flex align-center">
+        <v-icon
+          :icon="work_group_icon[auftrag.arbeitskreis_name]"
+          size="tiny"
+          class="me-2"
+          :class="`${arbeitskreis_name_to_css(auftrag.arbeitskreis_name)}`"
+        />
+        {{ auftrag.arbeitskreis_name }}
+
+      </div>
+    </template>
+    <template v-slot:subtitle>
+      {{ format_date(auftrag.abgabetermin) }}
+    </template>
+    <template v-slot:text>
+
+
+      <v-card
+        v-if="auftrag.text"
+        @click="text_dialog = true"
+        :class="`${arbeitskreis_name_to_css(auftrag.arbeitskreis_name)}-bg-dark`"
+        class="mb-3 hover_card">
+        <template v-slot:title>
+          <div class="d-flex align-center mb-1 text-subtitle-1">
+            <v-icon icon="mdi-pencil" size="tiny" class="me-2"/>
+            <div>
+              Zugeordneter Text:
+            </div>
+            <div class="flex-grow-1"></div>
+            <v-icon
+              icon="mdi-cursor-pointer"
+              size="tiny"
+            />
+          </div>
+        </template>
+        <template v-slot:subtitle>
+          {{ auftrag_type_to_name[auftrag.auftragsartText] }}
+        </template>
+        <template v-slot:text>
+          {{ auftrag.text.titel }}
+        </template>
+      </v-card>
+
+
+      <v-card v-if="auftrag.melodie" :class="`${arbeitskreis_name_to_css(auftrag.arbeitskreis_name)}-bg-dark`" class="mb-3" @click="melodie_dialog = true">
+        <template v-slot:title>
+          <div class="d-flex align-center mb-1 text-subtitle-1">
+            <v-icon icon="mdi-music-note" size="tiny" class="me-2"/>
+            <div>
+              Zugeordnete Melodie:
+            </div>
+            <div class="flex-grow-1"></div>
+            <v-icon
+              icon="mdi-cursor-pointer"
+              size="tiny"
+            />
+          </div>
+        </template>
+        <template v-slot:subtitle>
+          {{ auftrag_type_to_name[auftrag.auftragsartMelodie] }}
+        </template>
+        <template v-slot:text>
+          {{ auftrag.melodie.titel }}
+        </template>
+      </v-card>
+
+
+      <v-card v-if="auftrag.anmerkung" :class="`${arbeitskreis_name_to_css(auftrag.arbeitskreis_name)}-bg-dark`" >
+        <template v-slot:title>
+          <div class="d-flex align-center mb-1 text-subtitle-1" @click="text_dialog = true">
+            <v-icon icon="mdi-message-bulleted" size="tiny" class="me-2"/>
+            <div>
+              Anmerkung:
+            </div>
+          </div>
+        </template>
+        <template v-slot:text>
+          {{ auftrag.anmerkung }}
+        </template>
+      </v-card>
+
+    </template>
+  </v-card>
+
+  <v-dialog v-model="text_dialog" width="700">
+    <TextDialog :text="auftrag.text" />
+  </v-dialog>
+  <v-dialog v-model="melodie_dialog" width="700">
+    <MelodieDialog :melodie="auftrag.melodie" />
+  </v-dialog>
+</template>
+
+<script>
+import {useAppStore} from "@/store/app";
+import {work_group_icon, auftrag_type_to_name} from "@/assets/js/utils";
+import moment from "moment/moment";
+import TextDialog from "@/components/SongRelated/TextDialog.vue";
+import MelodieDialog from "@/components/SongRelated/MelodieDialog.vue";
+
+export default {
+  name: "AuftragCard",
+  components: {TextDialog, MelodieDialog},
+  data: () => ({
+    store: useAppStore(),
+    text_dialog: false,
+    melodie_dialog: false,
+  }),
+  props: {
+    auftrag: Object,
+  },
+  mounted() {
+    // console.log(this.auftrag)
+  },
+  computed: {
+    work_group_icon() {
+      return work_group_icon;
+    },
+    auftrag_type_to_name() {
+      return auftrag_type_to_name
+    }
+  },
+  methods: {
+    arbeitskreis_name_to_css(name) {
+      return name.replace('ß', 'ss').replace(' ', '-').toLowerCase()
+    },
+    arbeitskreis_name(id) {
+      return this.store.arbeitskreis_by_id(id);
+    },
+    format_date(date) {
+      if (date)
+        return moment(date).format("DD.MM.YYYY");
+      return 'Kein Datum'
+    },
+  }
+}
+</script>
+
+<style>
+.hover_card:hover {
+
+}
+
+</style>
