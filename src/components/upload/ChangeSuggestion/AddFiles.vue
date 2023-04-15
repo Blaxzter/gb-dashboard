7302917a61d8531f9b8935e09fb9f85d13fb0eb2
Frederic Marvin Abraham <mail@fabraham.dev>
2023-04-15 12:17:31 +0200

    Change some styles

---
+++ src/components/upload/ChangeSuggestion/AddFiles.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="text-h5 mb-5" v-if="melodie">
+    Lade neue Dateien hoch
+  </div>
+  <div v-if="!melodie">
+    <v-alert
+      color="primary"
+      icon="$warning"
+      title="Hinweis"
+      text="Das ausgewählte Gesangbuchlied hat keine Melodie zugeordnet. Eine Aktualisierung der Noten ist deshalb nicht möglich."
+    ></v-alert>
+  </div>
+  <div v-else>
+    <v-chip-group>
+      <v-chip v-for="(file, index) in melodie?.files" :key="index" prepend-icon="mdi-file-music" >
+        {{ file.filename_download }}
+      </v-chip>
+    </v-chip-group>
+    <v-file-input
+      v-model="noten"
+      show-size
+      label="Noten Datein"
+      multiple
+      counter
+      chips
+      @change="$emit('update:noten', noten)"
+    >
+    </v-file-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddFiles",
+  props: {
+    melodie: Object,
+  },
+  data: () => ({
+    noten: []
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/util/MenuList.vue
+++ src/components/util/MenuList.vue
@@ -1,6 +1,6 @@
 <template>
   <v-list :rounded="rounded ? 'lg' : 'none'" :lines="false" nav>
-    <v-list-item v-for="(item, i) in links" :key="i" :to="item.route" :class="{'ms-2': item.indent, 'mb-3': item.marginButton}">
+    <v-list-item v-for="(item, i) in links" :key="i" :to="item.route" :class="{'mb-3': item.marginButton}">
       <template v-slot:prepend v-if="item.icon">
         <v-icon :icon="item.icon" />
       </template>
@@ -28,48 +28,41 @@ export default {
         name: "Dashboard",
         route: "dashboard",
         icon: "mdi-home",
-        indent: false,
         marginButton: true,
       },
       {
         name: "Kalender",
         route: "kalender",
         icon: "mdi-calendar-month",
-        indent: false,
         marginButton: true,
       },
       {
         name: "Gesangbuch<wbr>lieder",
         route: "gesangbuchlieder",
         icon: "mdi-music",
-        indent: false,
         marginButton: true,
       },
       {
         name: "Arbeitsaufträge",
         route: "arbeitsauftraege",
         icon: "mdi-file-document-outline",
-        indent: false,
         marginButton: true,
       },{
         name: "Hochladen",
         route: "",
         icon: null,
-        indent: false,
         marginButton: false,
       },
       {
         name: "Gesangbuchlied Hochladen",
         route: "gesangbuchliedhochladen",
         icon: "mdi-upload",
-        indent: true,
         marginButton: false,
       },
       {
         name: "Änderung Eintragen",
         route: "aenderunghochladen",
         icon: "mdi-pencil",
-        indent: true,
         marginButton: true,
       },
     ],
