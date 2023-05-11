e12b77b8dedf78659b8fdd2ace055cee9a35849e
Frederic Marvin Abraham <mail@fabraham.dev>
2023-05-11 23:03:44 +0200

    Add kleiner kreis ansichts button, fix doughnut chart

--- src/layouts/new/AppBar.vue
+++ src/layouts/new/AppBar.vue
@@ -1,7 +1,8 @@
 <template>
   <v-app-bar flat class="my-app-bar header-banner">
     <v-container fluid class="main-container d-flex align-center">
-      <v-avatar image="/src/assets/images/logo.png" rounded="0" class="me-10 ms-4 dashboard-icon" @click="$router.push('Dashboard')"></v-avatar>
+      <v-avatar image="/src/assets/images/logo.png" rounded="0" class="me-10 ms-4 dashboard-icon"
+                @click="$router.push('Dashboard')"></v-avatar>
 
       <div class="d-flex flex-column align-baseline flex-md-row">
         <span class="text-h6 font-weight-bold mr-3">Gesangbuch 2026</span>
@@ -10,14 +11,52 @@
 
       <v-spacer></v-spacer>
 
-      <MenuComponent v-if="!$vuetify.display.lgAndUp" />
+      <!-- add tooltip  -->
+      <v-tooltip text="Aktiviere die Ansicht für den kleinen Kreis" location="bottom" v-if="is_kleiner_kreis">
+        <template v-slot:activator="{ props }">
+          <v-btn prepend-icon="mdi-record-circle-outline" class="me-4"
+                 variant="tonal"
+                 :color="is_kleiner_kreis_ansicht ? 'primary' : null" v-bind="props"
+                 @click="toggle_kleiner_kreis_ansicht">
+            Kleiner Kreis
+          </v-btn>
+        </template>
+      </v-tooltip>
+
+      <MenuComponent v-if="!$vuetify.display.lgAndUp"/>
     </v-container>
   </v-app-bar>
 </template>
 
-<script setup>
+<script>
 //
 import MenuComponent from "@/components/util/MenuComponent.vue";
+import {useUserStore} from "@/store/user";
+
+export default {
+  name: 'AppBar',
+  data: () => ({
+    userStore: useUserStore()
+  }),
+  components: {
+    MenuComponent
+  },
+  computed: {
+    is_kleiner_kreis_ansicht() {
+      return this.userStore.is_kleiner_kreis_ansicht
+    },
+    is_kleiner_kreis() {
+      return this.userStore.is_kleiner_kreis
+    }
+  },
+  methods: {
+    toggle_kleiner_kreis_ansicht() {
+      this.userStore.toggle_kleiner_kreis_ansicht()
+    }
+  }
+}
+
+
 </script>
 
 <style lang="scss">
