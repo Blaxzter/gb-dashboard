c48456f91e00b205f5cb00abc1bf7311658554ef
Frederic Abraham <mail@fabraham.dev>
2024-04-17 13:35:46 +0200

    fix bug with loading

--- src/layouts/new/DefaultLayout.vue
+++ src/layouts/new/DefaultLayout.vue
@@ -1,14 +1,35 @@
 <template>
   <v-app id="inspire">
-    <default-bar />
+    <template v-if="!currentState">
+      <LoaderComponent />
+    </template>
+    <template v-else>
+      <default-bar />
 
-    <default-view />
+      <default-view />
+    </template>
   </v-app>
 </template>
 
 <script setup>
 import DefaultBar from "./AppBar.vue";
 import DefaultView from "./MainView.vue";
+
+// get pinia store from useeAppStore
+import { useAppStore } from "@/store/app";
+import LoaderComponent from "@/components/util/LoaderComponent.vue";
+import { computed, onMounted } from "vue";
+
+const app = useAppStore();
+
+// computed that returns the current state
+const currentState = computed(() => {
+  return app.data_loaded;
+});
+
+onMounted(() => {
+  console.log(currentState.value);
+});
 </script>
 
 <style></style>
