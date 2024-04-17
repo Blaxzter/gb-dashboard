8878105fe8ea54a9878e0503a7776c29a7771011
Frederic Abraham <mail@fabraham.dev>
2024-04-17 13:35:46 +0200

    fix bug with loading

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,5 @@
 <template>
-  <LoaderComponent v-if="!currentState" />
-  <router-view v-else />
+  <router-view />
 </template>
 
-<script setup>
-// On mounted preform autologin
-import { computed, onMounted } from "vue";
-
-// get pinia store from useeAppStore
-import { useAppStore } from "@/store/app";
-import LoaderComponent from "@/components/util/LoaderComponent.vue";
-
-const app = useAppStore();
-
-// computed that returns the current state
-const currentState = computed(() => {
-  return app.data_loaded;
-});
-
-onMounted(() => {
-  console.log(app.data_loaded);
-});
-</script>
+<script setup></script>
