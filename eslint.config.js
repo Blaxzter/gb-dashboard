b0914ece10e05c9fb36a955d2bbac68027449e98
Frederic Abraham <mail@fabraham.dev>
2024-04-24 17:11:16 +0200

    fix auftrag table and eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,12 @@ export default [
   {
     rules: {
       "vue/no-v-html": "off",
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "auto",
+        },
+      ],
     },
   },
 ];
